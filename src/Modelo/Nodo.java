28d0cbfaa06a4f7662078f4600f58aadb37d5c2a
Juan <juanpablozabala21@gmail.com>
2019-10-04 10:46:18 -0400

    Modificando

---
+++ src/Modelo/Nodo.java
@@ -0,0 +1,35 @@
+
+package Modelo;
+
+/**
+ *
+ * @author Juan
+ */
+public class Nodo {
+    public Proceso dato; 
+    public Nodo siguiente;
+    public Nodo anterior;
+    public Nodo(Proceso dato) {
+        this.dato = dato;
+        this.siguiente=null;
+        this.anterior=null;
+    } 
+
+    public Proceso getDato() {
+        return dato;
+    }
+
+    public void setDato(Proceso dato) {
+        this.dato = dato;
+    }
+    
+    public Nodo getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Nodo anterior) {
+        this.anterior = anterior;
+    }
+
+    
+}
